4f00a15a5b26a0517fa7c5cf9ff9f107d8977d9a
dhslrl321 <dhslrl321@gmail.com>
2023-09-30 00:10:01 +0900

    comit

---
+++ src/test/kotlin/com/github/dhslrl321/alpha/AlphaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dhslrl321.alpha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
