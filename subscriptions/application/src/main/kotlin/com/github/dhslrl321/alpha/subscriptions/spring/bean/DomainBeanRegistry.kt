1b34fe13aed1368800f3fa21c3392a906de541cf
dhslrl321 <dhslrl321@gmail.com>
2023-11-08 23:51:01 +0900

    commit

--- subscriptions/application/src/main/kotlin/com/github/dhslrl321/alpha/subscriptions/spring/bean/DomainBeanRegistry.kt
+++ subscriptions/application/src/main/kotlin/com/github/dhslrl321/alpha/subscriptions/spring/bean/DomainBeanRegistry.kt
@@ -1,11 +1,12 @@
 package com.github.dhslrl321.alpha.subscriptions.spring.bean
 
 import com.github.dhslrl321.alpha.subscription.domain.SubscriptionFactory
+import com.github.dhslrl321.alpha.subscription.service.BillingCycleCalculator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class DomainBeanRegistry {
     @Bean
-    fun subscriptionFactory() = SubscriptionFactory()
+    fun subscriptionFactory() = SubscriptionFactory(BillingCycleCalculator())
 }
